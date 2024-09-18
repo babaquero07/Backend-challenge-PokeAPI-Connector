1766161633c5a9303d8a53edd2aafea92e13ecf6
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-18 10:42:56 -0500

    refactor(http): Refactor HttpCustomService error handling

--- src/providers/http/http.service.ts
+++ src/providers/http/http.service.ts
@@ -1,5 +1,9 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -10,13 +14,25 @@ export class HttpCustomService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
 
+      if (!response || response.status === 404) {
+        this.handleError(new NotFoundException('Resource not found.'));
+      }
+
       return response.data;
     } catch (error) {
-      console.log(error);
-
-      throw new InternalServerErrorException(
-        'An error occurred while trying to fetch data from the external API.',
-      );
+      this.handleError(error);
     }
+  }
+
+  private handleError(error: any): never {
+    if (error.response && error.response.status === 404) {
+      throw new NotFoundException('Resource not found.');
+    }
+
+    console.log(error);
+
+    throw new InternalServerErrorException(
+      'An error occurred while trying to fetch data from the external API.',
+    );
   }
 }
